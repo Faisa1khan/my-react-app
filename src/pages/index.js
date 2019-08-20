e4043f4bf544dd1776da4b333ef9edb3c850d4db
faisa1khan <faisalkhan5464@gmial.com>
2019-08-21 00:31:15 +0530

    second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">watch a vedio</Link>
+    <Link to="/page-2/">watch a vedio using this link</Link>
   </Layout>
 )
 
