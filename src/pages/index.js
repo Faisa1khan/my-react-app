b9d49ab711c993f38d24a5daf29291d300e1b4b5
faisa1khan <faisalkhan5464@gmial.com>
2019-08-21 00:28:30 +0530

    just checking

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,12 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Learn to design and code React apps</h1>
+    <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">watch a vedio</Link>
   </Layout>
 )
 
