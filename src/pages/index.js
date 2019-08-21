58dd77dda1701ec4e792dc8d48d84d6ccb56c36a
faisa1khan <faisalkhan5464@gmial.com>
2019-08-21 12:25:40 +0530

    header hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,21 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Learn to design and code React apps</h1>
-    <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+  <div>
+    <SEO title="gatsby" />
+    <div className="Hero">
+      <div className="HeroGroup">
+      <h1>Learn to design and code React apps</h1>
+      <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift</p>
+      
+      <Link to="/page-2/">Watch the video</Link>
+      </div>
     </div>
-    <Link to="/page-2/">watch a vedio using this link</Link>
-  </Layout>
+   
+  </div>
 )
 
 export default IndexPage
