4f30fa32e6386bd9fe72df85342ddaab7a2a6d7f
faisa1khan <faisalkhan5464@gmial.com>
2019-08-23 02:59:18 +0530

    static data redering

--- src/components/header.js
+++ src/components/header.js
@@ -4,17 +4,46 @@ import React from "react"
 import './Header.css'
 
 
-const Header = ({ siteTitle }) => (
-  <div className="Header">
-    <div className="HeaderGroup">
-       <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30" /></Link>
-       <Link to="/courses">Courses</Link>
-       <Link to="/downloads">Downloads</Link>
-       <Link to="/workshop">workshop</Link>
-       <Link to="/buy"><button>Buy</button></Link>
+
+class Header extends React.Component {
+ 
+  constructor(props) {
+    super(props)
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  componentDidMount(){
+    window.addEventListener('scroll', 
+    this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset;
+
+    if (scrollTop > 50) {
+      this.setState({hasScrolled: true })
+    }else {
+      this.setState({hasScrolled: false })
+    }
+  }
+
+  render() {
+    return (
+      <div className={this.state.hasScrolled ? 
+      'Header HeaderScrolled': 'Header'}>
+      <div className="HeaderGroup">
+         <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30" /></Link>
+         <Link to="/courses">Courses</Link>
+         <Link to="/downloads">Downloads</Link>
+         <Link to="/workshop">workshop</Link>
+         <Link to="/buy"><button>Buy</button></Link>
+      </div>
     </div>
-  </div>
-)
+    )
+  }
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,35 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Card from "../components/Card";
+import Section from "../components/Section";
+import Wave from "../components/Wave";
+import staticdata from "../../staticdata.json"
+import Cell from "../components/Cell";
+import styled from 'styled-components'
 
+
+const SectionCaption = styled.div`
+  font-weight: 600;
+  font-size: 18px;
+  text-transform: uppercase;
+  color: #94A4BA;
+  text-align: center;
+ 
+`
+const SectionCellGroup = styled.div`
+  max-width: 800px;
+  margin: 0 auto 100px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-column-gap: 20px;
+  padding: 0 20px;
+
+  @media (max-width: 800px) {
+  grid-template-columns: 1fr;
+
+  }
+
+`
 
 const IndexPage = () => (
   <Layout>
@@ -31,32 +59,7 @@ const IndexPage = () => (
         <img src={require('../images/logo-swift.png')} 
         width="50"/>
       </div>
-
-      <svg width="100%" height="172"  fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path fill="white">
-                <animate repeatCount="indefinite"
-                fill="freeze" attributeName="d" dur="10s"
-                values="M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818
-                -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007
-                2560 25.9088V171.91L0 171.91V25.9086Z;
-
-                M0 87.1596C316 87.1597 444 160 884 52.0001C1324 -55.9999 1320.29
-                34.966 1538 71.251C1814 117.251 2156 189.252 2560 87.1597V233.161L0
-                233.161V87.1596Z;
-
-                M0 53.6584C158 11.0001 213 0 363 0C513 0 855.555 115.001 1154 115.001C1440
-                115.001 1626 -38.0004 2560 53.6585V199.66L0 199.66V53.6584Z;
-
-                M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818
-                -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007
-                2560 25.9088V171.91L0 171.91V25.9086Z
-                
-                "
-                />
-            </path>
-      </svg>
-
-      
+          <Wave />
       </div>
     </div>
     <div className="Cards">
@@ -80,6 +83,24 @@ const IndexPage = () => (
           image={require('../images/wallpaper4.jpg')} />
       </div>
     </div>
+    <Section 
+     image={require('../images/wallpaper2.jpg')}
+     logo={require('../images/logo-react.png')}
+     title="React for Designers"
+     text="Learn how to build a modern site using React and the most efficient
+     libraries to get your site/product online. Get familiar with 
+     components, Grid Css, animations, interactions, dynamic data
+     with Contentfol and deploying your site with Netlify."
+    />
+    
+    <SectionCaption>12 section - 6 hours</SectionCaption>
+    <SectionCellGroup>
+      {staticdata.cells.map((cell) =>(
+        <Cell 
+          title={cell.title}
+          image={cell.image}/>
+      ))}
+    </SectionCellGroup>
   </Layout>
 )
 
